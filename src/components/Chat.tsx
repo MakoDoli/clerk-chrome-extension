5dd3dd063b3f424238c248e777a7539ad7ae787f
MakoDoli <makodolidze@hotmail.com>
2024-10-04 02:05:51 +0400

    add server url

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -35,13 +35,16 @@ const Chat = () => {
       //     messages: [{ role: "user", content: message }],
       //   }),
       // });
-      const res = await fetch("http://localhost:5000/openai", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ message }),
-      });
+      const res = await fetch(
+        "https://desolate-scrubland-86805-d0840db50656.herokuapp.com/openai",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message }),
+        }
+      );
       if (!res.ok) {
         throw new Error("Failed to fetch response from OpenAI");
       }
